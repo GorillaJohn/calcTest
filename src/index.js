70ed2fff593f56a6a782c790a907d796fcd6fed7
GorillaJohn <siyanko1994@mail.ru>
2021-11-24 00:15:00 +0300

    works

--- src/index.js
+++ src/index.js
@@ -3,23 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-/*window.CalcWidget = {
-  mount: (props, container) => {
-    ReactDOM.render(<App {...props} />, container,
-      document.getElementById('root'));
-
-  },
-  unmount: (container) => {
-    ReactDOM.unmountComponentAtNode(container);
-  }
-}
-
-window.CalcWidget.mount()*/
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('calculator')
+  document.getElementById('root')
 );
